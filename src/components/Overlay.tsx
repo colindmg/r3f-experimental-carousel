3e696be21a2572406f83a1e30448227f94c02a73
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2025-11-18 00:59:16 +0100

    Improving the Overlay for the demo

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -7,9 +7,60 @@ const Overlay = () => {
         <div className="absolute top-0 left-0 right-0 h-40 bg-linear-to-b from-[#f8f8f8] to-transparent z-5"></div>
         <div className="absolute bottom-0 left-0 right-0 h-40 bg-linear-to-t from-[#f8f8f8] to-transparent z-5"></div>
 
-        <h1 className="font-bold italic tracking-tighter z-10">
-          R3F EXPERIMENTAL CAROUSEL
-        </h1>
+        <div className="z-10 flex justify-between w-full">
+          <div>
+            <h1 className="font-bold italic tracking-tighter">
+              R3F EXPERIMENTAL CAROUSEL
+            </h1>
+            <div className="flex gap-3 items-center pointer-events-auto">
+              <a target="_blank" href="#">
+                Article
+              </a>
+              <a target="_blank" href="https://tympanus.net/codrops/demos/">
+                All demos
+              </a>
+              <a
+                target="_blank"
+                href="https://github.com/colindmg/r3f-experimental-carousel"
+              >
+                GitHub
+              </a>
+            </div>
+          </div>
+
+          <nav className="flex flex-col gap-2 pointer-events-auto text-sm items-end">
+            <a
+              target="_blank"
+              href="https://tympanus.net/codrops/demos/?tag=react-three-fiber"
+            >
+              #react-three-fiber
+            </a>
+            <a
+              target="_blank"
+              href="https://tympanus.net/codrops/demos/?tag=three-js"
+            >
+              #three-js
+            </a>
+            <a
+              target="_blank"
+              href="https://tympanus.net/codrops/demos/?tag=shader"
+            >
+              #shader
+            </a>
+            <a
+              target="_blank"
+              href="https://tympanus.net/codrops/demos/?tag=glsl"
+            >
+              #glsl
+            </a>
+            <a
+              target="_blank"
+              href="https://tympanus.net/codrops/demos/?tag=3d"
+            >
+              #3d
+            </a>
+          </nav>
+        </div>
         <div className="z-10 uppercase flex gap-5 tracking-tighter pointer-events-auto">
           <NavLink
             to="/"
