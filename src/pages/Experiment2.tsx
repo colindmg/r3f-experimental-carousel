4aa4d22094f748e19d8ce3ce96a3ac9989b59503
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2025-11-18 01:02:20 +0100

    Adding a loader to the different pages

--- src/pages/Experiment2.tsx
+++ src/pages/Experiment2.tsx
@@ -1,49 +1,56 @@
+import { Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 import Carousel from "../components/Carousel";
 
 const Experiment2 = () => {
   return (
-    <Canvas
-      style={{
-        position: "fixed",
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-      }}
-      camera={{
-        position: [0, 0, 3],
-      }}
-    >
-      <Carousel
-        imageSize={[2, 0.4]}
-        gap={0.1}
-        position={[2.5, 0, 0]}
-        curveFrequency={0.3}
-        curveStrength={-1.2}
-        wheelFactor={0.5}
-        wheelDirection={-1}
-      />
-      <Carousel
-        imageSize={[2, 0.4]}
-        gap={0.1}
-        position={[-2.5, 0, 0]}
-        curveFrequency={0.3}
-        curveStrength={1.2}
-        wheelFactor={0.5}
-        wheelDirection={-1}
-      />
+    <>
+      <Canvas
+        style={{
+          position: "fixed",
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+        }}
+        camera={{
+          position: [0, 0, 3],
+        }}
+      >
+        <Suspense fallback={null}>
+          <Carousel
+            imageSize={[2, 0.4]}
+            gap={0.1}
+            position={[2.5, 0, 0]}
+            curveFrequency={0.3}
+            curveStrength={-1.2}
+            wheelFactor={0.5}
+            wheelDirection={-1}
+          />
+          <Carousel
+            imageSize={[2, 0.4]}
+            gap={0.1}
+            position={[-2.5, 0, 0]}
+            curveFrequency={0.3}
+            curveStrength={1.2}
+            wheelFactor={0.5}
+            wheelDirection={-1}
+          />
 
-      <Carousel
-        imageSize={[1, 1.5]}
-        gap={0.05}
-        position={[0, 0, 0]}
-        curveFrequency={0}
-        curveStrength={0}
-        wheelFactor={0.25}
-        wheelDirection={1}
-      />
-    </Canvas>
+          <Carousel
+            imageSize={[1, 1.5]}
+            gap={0.05}
+            position={[0, 0, 0]}
+            curveFrequency={0}
+            curveStrength={0}
+            wheelFactor={0.25}
+            wheelDirection={1}
+          />
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   );
 };
 
--- src/pages/Experiment3.tsx
+++ src/pages/Experiment3.tsx
@@ -1,55 +1,62 @@
+import { Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 import Carousel from "../components/Carousel";
 
 const Experiment3 = () => {
   return (
-    <Canvas
-      style={{
-        position: "fixed",
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-      }}
-      camera={{
-        position: [0, 0, 3],
-        rotation: [0, 0, Math.PI / 7],
-      }}
-    >
-      <Carousel
-        imageSize={[0.8, 1]}
-        gap={0}
-        wheelFactor={0.2}
-        position={[-1.4, 0, 0]}
-        curveFrequency={0.4}
-        curveStrength={0.9}
-      />
-      <Carousel
-        imageSize={[0.8, 1]}
-        gap={0}
-        wheelFactor={0.3}
-        position={[-0.7, 0, 0]}
-        curveFrequency={0.4}
-        curveStrength={0.6}
-      />
-      <Carousel imageSize={[0.8, 1]} gap={0} wheelFactor={0.4} />
-      <Carousel
-        imageSize={[0.8, 1]}
-        gap={0}
-        wheelFactor={0.5}
-        position={[0.7, 0, 0]}
-        curveFrequency={0.4}
-        curveStrength={-0.6}
-      />
-      <Carousel
-        imageSize={[0.8, 1]}
-        gap={0}
-        wheelFactor={0.6}
-        position={[1.4, 0, 0]}
-        curveFrequency={0.4}
-        curveStrength={-0.9}
-      />
-    </Canvas>
+    <>
+      <Canvas
+        style={{
+          position: "fixed",
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+        }}
+        camera={{
+          position: [0, 0, 3],
+          rotation: [0, 0, Math.PI / 7],
+        }}
+      >
+        <Suspense fallback={null}>
+          <Carousel
+            imageSize={[0.8, 1]}
+            gap={0}
+            wheelFactor={0.2}
+            position={[-1.4, 0, 0]}
+            curveFrequency={0.4}
+            curveStrength={0.9}
+          />
+          <Carousel
+            imageSize={[0.8, 1]}
+            gap={0}
+            wheelFactor={0.3}
+            position={[-0.7, 0, 0]}
+            curveFrequency={0.4}
+            curveStrength={0.6}
+          />
+          <Carousel imageSize={[0.8, 1]} gap={0} wheelFactor={0.4} />
+          <Carousel
+            imageSize={[0.8, 1]}
+            gap={0}
+            wheelFactor={0.5}
+            position={[0.7, 0, 0]}
+            curveFrequency={0.4}
+            curveStrength={-0.6}
+          />
+          <Carousel
+            imageSize={[0.8, 1]}
+            gap={0}
+            wheelFactor={0.6}
+            position={[1.4, 0, 0]}
+            curveFrequency={0.4}
+            curveStrength={-0.9}
+          />
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   );
 };
 
